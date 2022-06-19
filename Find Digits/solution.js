d0bd7907829788470804bd998cb31e3f89b954bd
Spyros Babalitis <spyros.babalitis@gmail.com>
2022-06-19 14:50:44 +0200

    Find Digits
    Javascript (Node.js)

---
+++ Find Digits/solution.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function findDigits(n) {
+    // Solid solution, uses forEach too.
+    let chars = n.toString().split('');
+    let result = 0;
+    chars.forEach(element => {
+        if(n%element==0){
+            result++;
+        }
+    });
+    return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = findDigits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
